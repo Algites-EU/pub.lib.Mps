4afee52bba11d20a8f234ef841d9438511466c71
Artur Linhart <Artur.Linhart@algites.eu>
2026-06-06 21:28:02 +0200

    usage of central build geadle scriptsa improved, groupId introduced to yml files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,2 @@
 
-// old no loinger explicitly called, is now called from common repository scripts:
-// apply(from = uri("https://raw.githubusercontent.com/Algites-EU/pub.gov.Algites/main/gradle/tool/documentation/algites-docs-site-mps.gradle.kts"))
 apply(from = uri("https://raw.githubusercontent.com/Algites-EU/pub.gov.Algites/main/gradle/tool/repository/algites-root-build.gradle.kts"))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,57 @@
+val locIsCi: Boolean =
+    providers.gradleProperty("CI")
+        .orElse(providers.environmentVariable("CI"))
+        .map { it.equals("true", ignoreCase = true) }
+        .orElse(false)
+        .get()
+
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
+        if (!locIsCi) {
+            mavenLocal()
+        }
         mavenCentral()
+        maven {
+            name = "algites-public-releases"
+            url = uri("https://repo1.maven.org/maven2")
+            mavenContent {
+                releasesOnly()
+            }
+        }
+        maven {
+            name = "algites-public-snapshots"
+            url = uri("https://dl.cloudsmith.io/public/algites/maven-snapshots-pub/")
+            mavenContent {
+                snapshotsOnly()
+            }
+        }
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        mavenLocal()
+        if (!locIsCi) {
+            mavenLocal()
+        }
         mavenCentral()
+        maven {
+            name = "algites-public-releases"
+            url = uri("https://repo1.maven.org/maven2")
+            mavenContent {
+                releasesOnly()
+            }
+        }
+        maven {
+            name = "algites-public-snapshots"
+            url = uri("https://dl.cloudsmith.io/public/algites/maven-snapshots-pub/maven/")
+            mavenContent {
+                snapshotsOnly()
+            }
+        }
     }
 }
 
-rootProject.name = "pub.lib.Mps"
-
 apply(from = uri("https://raw.githubusercontent.com/Algites-EU/pub.gov.Algites/main/gradle/tool/repository/algites-root-settings-discovery.gradle.kts"))
-
