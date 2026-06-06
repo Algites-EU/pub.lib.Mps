66d1f924dc34153465c17459ce92639031c2abba
Artur Linhart <Artur.Linhart@algites.eu>
2026-06-06 14:37:32 +0200

    gradle build for whole repo modified

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,12 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        mavenLocal()
         mavenCentral()
     }
 }
 
 rootProject.name = "pub.lib.Mps"
+
+apply(from = uri("https://raw.githubusercontent.com/Algites-EU/pub.gov.Algites/main/gradle/tool/repository/algites-root-settings-discovery.gradle.kts"))
+
